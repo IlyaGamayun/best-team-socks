9c04252b382e3b1d47de911873e49843eed84619
Vanya <geo.vox95@gmail.com>
2022-11-09 13:54:53 +0300

    add blocks, little css changes

--- views/Main.jsx
+++ views/Main.jsx
@@ -7,17 +7,18 @@ function Main({ title }) {
       <div className="slogan">
         Есть idea - есть soсkskea!
       </div>
-      <div className="soksBody" />
       <h2>Добро пожаловать в магазин потных носков!</h2>
-      <img src="/img/nosok.jpg" width="250" alt="nosok" />
-      <br />
-      Надоели ваши старые грязные и дырявые носки?
-      Выход есть - мы предлагаем вам замутить свои кастомные носки, заходи в
-      конструктор по кнопке и решай, как должен выглядеть твой идеальный мега-носок!
-      <form action="/socks/new">
-        <button type="submit">GO</button>
-      </form>
-      <div />
+      <div className="contentBody">
+        <img className="image" src="/img/nosok.jpg" width="250" alt="nosok" />
+        <div className="imageText">
+          Надоели ваши старые грязные и дырявые носки?
+          Выход есть - мы предлагаем вам замутить свои кастомные носки, заходи в
+          конструктор по кнопке и решай, как должен выглядеть твой идеальный мега-носок!
+          <form action="/socks/new">
+            <button type="submit">GO</button>
+          </form>
+        </div>
+      </div>
 
     </Layout>
   );
