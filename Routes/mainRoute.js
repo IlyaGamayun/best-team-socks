670cf41d82b805f858346b2727ea50f89a69d977
Vanya <geo.vox95@gmail.com>
2022-11-09 15:05:51 +0300

    some changes)))))

--- Routes/mainRoute.js
+++ Routes/mainRoute.js
@@ -3,7 +3,7 @@ const Main = require('../views/Main');
 
 router
   .get('/', (req, res) => {
-    res.renderComponent(Main, { title: 'Home page' });
+    res.renderComponent(Main, { title: 'Sokskea' });
   });
 
 module.exports = router;
